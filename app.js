a807c62af840b507512881512671bdcbe5b01291
sallyyou <workyooll@gmail.com>
2021-04-25 10:22:15 +0800

    pull request run deployment step only for main branch

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/health", (req, res) => {
   res.send("ok");
 });
 app.get("/version", (req, res) => {
-  res.send("1"); // change this string to ensure a new version deployed
+  res.send("v7"); // change this string to ensure a new version deployed
 });
 app.listen(PORT, () => {
   console.log("server started on port 5000"); // eslint-disable-line no-console
